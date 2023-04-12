cf4469d8d7c4f79f2253507925917231844b13a5
maria-jl <maria.law@ucalgary.ca>
2023-04-11 19:03:26 -0600

    merging changes

--- Models/Regression - ExtraTrees - Brian.py
+++ Models/Regression - ExtraTrees - Brian.py
@@ -18,18 +18,24 @@ et_reg = ExtraTreesRegressor(n_estimators=100, max_depth=10, random_state=42)
 et_reg.fit(X_train, y_train)
 
 # Make predictions on the testing data
-y_pred = et_reg.predict(X_test)
+y_pred_test = et_reg.predict(X_test)
+y_pred_train = et_reg.predict(X_train)
 
 # Evaluate the model's performance on the testing data
-mse = mean_squared_error(y_test, y_pred)
-r2 = r2_score(y_test, y_pred)
+mse_test = mean_squared_error(y_test, y_pred_test)
+r2_test = r2_score(y_test, y_pred_test)
+
+mse_train = mean_squared_error(y_train, y_pred_train)
+r2_train = r2_score(y_train, y_pred_train)
 
 # Print the performance metrics
-print('Mean squared error:', mse)
-print('R-Squared:', r2)
+print('Train Mean squared error:', mse_train)
+print('Train R-Squared:', r2_train)
+print('Test Mean squared error:', mse_test)
+print('Test R-Squared:', r2_test)
 
 # Plot the predicted values against the actual values
-plt.scatter(y_test, y_pred)
+plt.scatter(y_test, y_pred_test)
 plt.xlabel('Actual')
 plt.ylabel('Predicted')
 plt.title('ExtraTrees Scatter Plot')
